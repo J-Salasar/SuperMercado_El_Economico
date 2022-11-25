f2b535dfbaf854baf11554806a9889558086e35f
fernando colomer <colomerfernando1@gmail.com>
2022-11-25 09:07:04 -0600

    Muestra el usuario logueado en el activity iniciar

--- app/src/main/java/com/example/supermercado/ActivityIniciar.java
+++ app/src/main/java/com/example/supermercado/ActivityIniciar.java
@@ -2,13 +2,22 @@ package com.example.supermercado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ActivityIniciar extends AppCompatActivity {
-
+public static final String usuarios = "user";
+public TextView bienvendio;
+public String usuariologueado;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar);
+
+        bienvendio=(TextView) findViewById(R.id.txtBienvendioUser);
+        usuariologueado=getIntent().getStringExtra("user");
+        bienvendio.setText(usuariologueado);
     }
 }
