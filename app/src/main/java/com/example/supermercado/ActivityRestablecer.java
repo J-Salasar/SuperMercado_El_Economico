b95663816ff6a9487a460ddcd107a68bac91c7a7
fernando colomer <colomerfernando1@gmail.com>
2022-11-25 17:51:22 -0600

    disenio back

--- app/src/main/java/com/example/supermercado/ActivityRestablecer.java
+++ app/src/main/java/com/example/supermercado/ActivityRestablecer.java
@@ -2,13 +2,24 @@ package com.example.supermercado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class ActivityRestablecer extends AppCompatActivity {
-
+public ImageView imgRegresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restablecer);
+
+        imgRegresar=(ImageView) findViewById(R.id.imgVolver);
+        imgRegresar.setOnClickListener(this::onClickVolver);
+    }
+
+    private void onClickVolver(View view) {
+        startActivity(new Intent(ActivityRestablecer.this, MainActivity.class));
     }
 }
