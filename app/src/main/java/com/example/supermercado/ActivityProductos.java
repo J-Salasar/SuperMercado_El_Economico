22531b78a0554bea0e5481f185103ff67f475381
Salasar001 <salasar1820@gmail.com>
2022-11-29 18:50:27 -0600

    Buscardor implementado

--- app/src/main/java/com/example/supermercado/ActivityProductos.java
+++ app/src/main/java/com/example/supermercado/ActivityProductos.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -20,6 +23,9 @@ import com.example.supermercado.configuracion.productos;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ActivityProductos extends AppCompatActivity {
     private ArrayList<productos> productoslista;
     private ArrayList<String> id;
@@ -29,6 +35,7 @@ public class ActivityProductos extends AppCompatActivity {
     private ArrayList<String> foto;
     private ListView lista;
     private String usuario,rango;
+    private EditText buscar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +43,7 @@ public class ActivityProductos extends AppCompatActivity {
         lista=(ListView) findViewById(R.id.lista1_basedatos);
         usuario=getIntent().getStringExtra("user");
         rango=getIntent().getStringExtra("rango");
+        buscar=(EditText) findViewById(R.id.buscador_texto);
         ObtenerLista1();
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -50,6 +58,16 @@ public class ActivityProductos extends AppCompatActivity {
                 startActivity(modificar);*/
             }
         });
+    }
+    public void buscar_producto(View view){
+        if(buscar.getText().toString()==null){
+            ObtenerLista1();
+        }
+        else{
+            if(buscar.getText().toString()!=null){
+                ObtenerLista2();
+            }
+        }
     }
     private void ObtenerLista1() {
         StringRequest stringRequest=new StringRequest(Request.Method.GET, "http://apk.salasar.xyz:25565/productos.php?codigo=0123456789", new Response.Listener<String>(){
@@ -74,7 +92,42 @@ public class ActivityProductos extends AppCompatActivity {
                     fllList();
                 }
                 catch (Throwable error){
-                    Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "No nada", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "No nada", Toast.LENGTH_LONG).show();
+            }
+        });
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+    private void ObtenerLista2() {
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, "http://apk.salasar.xyz:25565/productos_buscar.php", new Response.Listener<String>(){
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONObject jsonObject=new JSONObject(response);
+                    JSONArray contactoarray=jsonObject.getJSONArray("productos");
+                    productos comida=null;
+                    productoslista=new ArrayList<productos>();
+                    for(int i=0;i<contactoarray.length();i++){
+                        JSONObject rowcontacto=contactoarray.getJSONObject(i);
+                        comida=new productos(
+                                rowcontacto.getString("id"),
+                                rowcontacto.getString("nombre"),
+                                rowcontacto.getString("precio"),
+                                rowcontacto.getString("cantidad"),
+                                rowcontacto.getString("foto")
+                        );
+                        productoslista.add(comida);
+                    }
+                    fllList();
+                }
+                catch (Throwable error){
+                    Toast.makeText(getApplicationContext(), "No nada con esa letra", Toast.LENGTH_LONG).show();
                 }
             }
         }, new Response.ErrorListener() {
@@ -82,7 +135,13 @@ public class ActivityProductos extends AppCompatActivity {
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
             }
-        });
+        }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("buscar", buscar.getText().toString());
+                return parametros;
+            }
+        };
         RequestQueue requestQueue= Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
     }
@@ -110,6 +169,7 @@ public class ActivityProductos extends AppCompatActivity {
             );
         }
         lista.setAdapter(new Adaptador(this,id,nombre,precio,cantidad,foto));
+
     }
     public void volver90(View view){
         if(rango.equals("Usuario")) {
